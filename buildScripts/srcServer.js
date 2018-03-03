87f0e5cfc58a8b70454a5a117e93c00c96392fa3
EMMETT\Emmett Brogan <emmett-b@hotmail.com>
2018-03-03 14:58:13 +0000

    bundling and linting added (linting = live code error report in console)

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,9 +2,21 @@ import express from 'express';
 import path from 'path';
 import open from 'open';
 
+//From bundling tutorial later
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
+
 const port = 3000;
 //Create instance of express server
 const app = express();
+
+//From bundling tutorial later
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
 
 //read from file where to load up first page
 app.get('/', function(req, res) {
