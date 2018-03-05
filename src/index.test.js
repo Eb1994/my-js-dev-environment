20e9f665661ffcb6c3034071d1bc69b6ecc0c75c
EMMETT\Emmett Brogan <emmett-b@hotmail.com>
2018-03-05 12:41:15 +0000

    http request created GET for populating table using mock data

--- src/index.test.js
+++ src/index.test.js
@@ -10,12 +10,24 @@ describe('Our first test', function() {
 });
 
 //Async test (DOM test + using jsdom to test dom elements) below: use 'done' below to make sure mocha knows it is done, otherwise it will always pass
+// describe('index.html', function() {
+//   it('should say hello', function(done) {
+//     const index = fs.readFileSync('./src/index.html', "utf-8");
+//     jsdom.env(index, function(err, window) {
+//       const h1 = window.document.getElementsByTagName('h1')[0];
+//       expect(h1.innerHTML).to.equal("Hello World!");
+//       done();
+//       window.close();
+//     });
+//   });
+// });
+
 describe('index.html', function() {
-  it('should say hello', function(done) {
+  it('should have h1 that says Users', function(done) {
     const index = fs.readFileSync('./src/index.html', "utf-8");
     jsdom.env(index, function(err, window) {
       const h1 = window.document.getElementsByTagName('h1')[0];
-      expect(h1.innerHTML).to.equal("Hello World!");
+      expect(h1.innerHTML).to.equal("Users");
       done();
       window.close();
     });
