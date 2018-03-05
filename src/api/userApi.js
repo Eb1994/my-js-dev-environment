cb618707b08d3d751ae6acebf26be2114b64a347
EMMETT\Emmett Brogan <emmett-b@hotmail.com>
2018-03-05 14:14:03 +0000

    setup sample API for mock data with proper simulation for requests GET and DELETE

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,28 @@
 import 'whatwg-fetch';
+import getBaseUrl from './baseUrl';
+
+const baseUrl = getBaseUrl();
 
 export function getUsers() {
   return get('users');
 }
 
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
+
 function get(url) {
-  return fetch(url).then(onSuccess, onError);
+  return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+//Cannot call function 'delete' since reserved work
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
